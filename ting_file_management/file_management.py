df75adee26b5fe592df7744f3ff82268c138d4d9
Pietro Bucker <pietro_177@hotmail.com>
2023-07-14 16:30:38 -0300

    req 2 completo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file[-3:] == "txt":
+        try:
+            with open(path_file, mode="r", encoding="UTF-8") as file:
+                content = file.read().split("\n")
+                return content
+        except FileNotFoundError:
+            error_message = f"Arquivo {path_file} não encontrado"
+            return print(error_message, file=sys.stderr)
+    return print("Formato inválido", file=sys.stderr)
+
+
+# txt_importer("statics/arquivo_teste.txt")
