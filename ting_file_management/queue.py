988a330fc7eb82065ea8c56a6f23ff25674ad399
Pietro Bucker <pietro_177@hotmail.com>
2023-07-14 15:10:28 -0300

    inicializando projeto req 1 completo

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,26 @@
 from ting_file_management.abstract_queue import AbstractQueue
 
+# o ideal ao invez de lista comum é usar a lista encadeada duplamente
+# porem como o projeto atual nao vai usar quantidades absurdas de dados
+# optei pela lista normal
+
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) == 0:
+            return None
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self.queue):
+            return self.queue[index]
+        raise IndexError("Índice Inválido ou Inexistente")
